ac776b36c63e69d4629ca543f148b88f018019fa
itasworkshop <itasworkshop@gmail.com>
2022-02-03 17:48:47 +0530

    02 Feb 2

--- interviews/examples/src/main/java/org/myexamples/Calculator.java
+++ interviews/examples/src/main/java/org/myexamples/Calculator.java
@@ -1,13 +1,32 @@
 package org.myexamples;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calculator {
+
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
 
     public int addition(int a,int b){
         return a+b;
     }
 
-    public static void main(String[] args){
+    public int division(int a,int b){
+        return a/b;
+    }
+
+    public static void main(String[] args) {
         Calculator obj = new Calculator();
-        System.out.println(obj.addition(25,35));
+        System.out.println(obj.addition(25, 35));
+        //System.out.println(obj.division(25,0));
+        logger.debug("Performing Subtraction --->");
+        logger.debug(obj.division(25, 5));
+        //logger.error(obj.division(25,0));
+
+        try {
+            obj.division(25, 0);
+        } catch (ArithmeticException e) {
+            logger.error("{}", e.getMessage());
+        }
     }
 }
